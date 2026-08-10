4c3648871db5661ea70ed7def7cf245ccf7b2fa0
João Pedro Peres Portela Garcia <pedrogarcya15@gmail.com>
2026-08-10 08:42:42 -0300

    Update ordenacao_vetores.c
    Ordena números em vetor

--- ordenacao_vetores.c
+++ ordenacao_vetores.c
@@ -3,15 +3,12 @@
 int main() {
     int i, V[3], original[3], aux;
 
-    // Lê os 3 números e guarda uma cópia da ordem original
     for (i = 0; i < 3; i++) {
         scanf("%d", &V[i]);
         original[i] = V[i];
     }
 
-    // Ordena os números em ordem crescente
     for (i = 0; i < 3; i++) {
-
         if (V[1] < V[0]) {
             aux = V[1];
             V[1] = V[0];
@@ -31,24 +28,17 @@ int main() {
         }
     }
 
-    // Imprime os números ordenados
+    // Números ordenados
     for (i = 0; i < 3; i++) {
         printf("%d\n", V[i]);
     }
 
-    // Linha em branco
     printf("\n");
 
-    // Imprime os números na ordem original
+    // Números na ordem original, um por linha
     for (i = 0; i < 3; i++) {
-        printf("%d", original[i]);
-
-        if (i < 2) {
-            printf(" ");
-        }
+        printf("%d\n", original[i]);
     }
-
-    printf("\n");
 
     return 0;
 }
