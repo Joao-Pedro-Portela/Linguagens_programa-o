6eaca5ff14e0c188ba60c12695bdc063a748dbaf
João Pedro Peres Portela Garcia <pedrogarcya15@gmail.com>
2026-09-24 15:45:25 -0300

    Abcdario da xuxa pedido pelo professor

---
+++ Abcedário da xuxa.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+int main() {
+
+    char letra;
+
+    printf("Digite uma letra de A a Z: ");
+    scanf("%c", &letra);
+
+    if (letra == 'A' || letra == 'a') {
+        printf("A de Amor");
+    }
+    else if (letra == 'B' || letra == 'b') {
+        printf("B de Brincadeira");
+    }
+    else if (letra == 'C' || letra == 'c') {
+        printf("C de Criança");
+    }
+    else if (letra == 'D' || letra == 'd') {
+        printf("D de Diversão");
+    }
+    else if (letra == 'E' || letra == 'e') {
+        printf("E de Escola");
+    }
+    else if (letra == 'F' || letra == 'f') {
+        printf("F de Felicidade");
+    }
+    else if (letra == 'G' || letra == 'g') {
+        printf("G de Gente");
+    }
+    else if (letra == 'H' || letra == 'h') {
+        printf("H de Hoje");
+    }
+    else if (letra == 'I' || letra == 'i') {
+        printf("I de Imaginação");
+    }
+    else if (letra == 'J' || letra == 'j') {
+        printf("J de Jogo");
+    }
+    else if (letra == 'K' || letra == 'k') {
+        printf("K de Kilo");
+    }
+    else if (letra == 'L' || letra == 'l') {
+        printf("L de Liberdade");
+    }
+    else if (letra == 'M' || letra == 'm') {
+        printf("M de Música");
+    }
+    else if (letra == 'N' || letra == 'n') {
+        printf("N de Natureza");
+    }
+    else if (letra == 'O' || letra == 'o') {
+        printf("O de Alegria");
+    }
+    else if (letra == 'P' || letra == 'p') {
+        printf("P de Paz");
+    }
+    else if (letra == 'Q' || letra == 'q') {
+        printf("Q de Qualidade");
+    }
+    else if (letra == 'R' || letra == 'r') {
+        printf("R de Respeito");
+    }
+    else if (letra == 'S' || letra == 's') {
+        printf("S de Sorriso");
+    }
+    else if (letra == 'T' || letra == 't') {
+        printf("T de Ternura");
+    }
+    else if (letra == 'U' || letra == 'u') {
+        printf("U de União");
+    }
+    else if (letra == 'V' || letra == 'v') {
+        printf("V de Vida");
+    }
+    else if (letra == 'W' || letra == 'w') {
+        printf("W de Web");
+    }
+    else if (letra == 'X' || letra == 'x') {
+        printf("X de Xadrez");
+    }
+    else if (letra == 'Y' || letra == 'y') {
+        printf("Y de Yoga");
+    }
+    else if (letra == 'Z' || letra == 'z') {
+        printf("Z de Zelo");
+    }
+    else {
+        printf("Letra invalida!");
+    }
+
+    return 0;
+}
